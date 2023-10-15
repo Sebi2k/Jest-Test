57d2d4ccd0391ffb52df1e0b3a66656becec6d0c
Sebi2k <96271751+Sebi2k@users.noreply.github.com>
2023-10-14 22:13:34 -0700

    step 8

--- sample-functions.js
+++ sample-functions.js
@@ -2,4 +2,91 @@ function mySum(a, b) {
   return a + b;
 }
 
-export default { mySum };
+class Stock {
+  constructor(symbol, quantity) {
+    this.symbol = symbol;
+    this.quantity = quantity;
+  }
+
+}
+
+class ShareSaleException extends Error {
+  constructor(message) {
+    super(message);
+    this.name = 'ShareSaleException';
+  }
+}
+
+class Portfolio{
+  constructor(name) {
+    this.name = name;
+    this.stocks = [];
+  }
+
+  addStock(stock) {
+    if (stock instanceof Stock) {
+      this.stocks.push(stock);
+    } else {
+      console.log("Invalid stock object. Please provide a valid Stock instance.");
+    }
+  }
+
+  isEmpty() {
+    return this.stocks.length === 0
+  }
+
+  getUniqueTickerCount() {
+    const uniqueTickers = new Set();
+    for (const stock of this.stocks) {
+      uniqueTickers.add(stock.symbol);
+    }
+    return uniqueTickers.size;
+  }
+
+  purchase(symbol, quantity) {
+    const existingStock = this.stocks.find((stock) => stock.symbol === symbol);
+
+    if (existingStock) {
+      existingStock.quantity += quantity;
+    } else {
+      const newStock = new Stock(symbol, quantity);
+      this.addStock(newStock);
+    }
+  }
+
+  sell(symbol, quantity) {
+    const existingStock = this.stocks.find((stock) => stock.symbol === symbol);
+
+    if (existingStock) {
+      if (existingStock.quantity >= quantity) {
+        existingStock.quantity -= quantity;
+        if (existingStock.quantity === 0) {
+          // Remove the stock if the quantity becomes zero
+          this.stocks = this.stocks.filter((stock) => stock.symbol !== symbol);
+        }
+      } else {
+        throw new ShareSaleException("Not enough shares to sell.");
+      }
+    } else {
+      throw new ShareSaleException("Stock not found in the portfolio.");
+    }
+  }
+
+  shares(symbol) {
+    const existingStock = this.stocks.find((stock) => stock.symbol === symbol);
+
+    if (existingStock) {
+      return existingStock.quantity;
+    }
+    else {
+      console.log("Stock not found in the portfolio.");
+    }
+  }
+
+
+}
+ 
+
+
+
+export default { mySum, Portfolio, Stock, ShareSaleException };
--- sample-functions.test.js
+++ sample-functions.test.js
@@ -5,3 +5,84 @@ test("Testing mySum -- success", () => {
   const result = myFunctions.mySum(12, 18);
   expect(expected).toBe(result);
 });
+
+test('Testing portfolio', () => {
+  const e1 = new myFunctions.Stock("G", 1);
+
+  const p = new myFunctions.Portfolio("me");
+  p.addStock(e1);
+  expect(e1).toBe(p.stocks[0]);
+
+});
+
+test('Testing portfolio isEmpty', () => {
+  const p = new myFunctions.Portfolio("me");
+  expect(p.isEmpty()).toBe(true);
+});
+
+test('Testing portfolio ticker', () => {
+  const e = new myFunctions.Stock("G", 1);
+  const e1 = new myFunctions.Stock("R", 1);
+  const e2 = new myFunctions.Stock("G", 1);
+
+  const p = new myFunctions.Portfolio("me");
+  p.addStock(e);
+  p.addStock(e1);
+  p.addStock(e2);
+
+  expect(p.getUniqueTickerCount()).toBe(2);
+});
+
+test('Testing portfolio purchase', () => {
+  const p = new myFunctions.Portfolio("me");
+  p.purchase("google", 3);
+  p.purchase("google", 1);
+
+  const s = "google";
+  const q = 4;
+  expect(p.stocks[0].symbol).toBe("google");
+  expect(p.stocks[0].quantity).toBe(4);
+
+});
+
+test('test for sell', () => {
+  const p = new myFunctions.Portfolio("me");
+  p.purchase("google", 3);
+  p.purchase("g", 2);
+  p.sell("google", 2)
+  p.sell("g", 1)
+
+  expect(p.stocks[0].quantity).toBe(1);
+  expect(p.stocks[1].quantity).toBe(1);
+})
+
+test('Testing portfolio shares', () => {
+  const p = new myFunctions.Portfolio("me");
+  p.purchase("google", 3);
+  p.purchase("google", 1);
+
+  expect(p.shares("google")).toBe(4);
+});
+
+test('Testing portfolio del', () => {
+  const p = new myFunctions.Portfolio("me");
+  p.purchase("google", 3);
+  p.purchase("google", 1);
+  p.sell("google", 4)
+  expect(String(p.stocks)).toBe('');
+});
+
+test('Testing portfolio too many', () => {
+  const myPortfolio = new myFunctions.Portfolio("My Portfolio");
+  myPortfolio.purchase("GME", 5);
+
+  try {
+    myPortfolio.sell("GME", 10);
+  } catch (error) {
+    if (error instanceof myFunctions.ShareSaleException) {
+      console.log("Error:", error.message);
+    } else {
+      console.log("An unexpected error occurred.");
+    }
+  }
+});
